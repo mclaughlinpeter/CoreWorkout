afb9767b394240614e55f3fafd92403d3c22310e
mclaughlinpeter <p.f.mclaughlin@hotmail.co.uk>
2017-12-02 10:11:13 -0500

    Add Extensions project

---
+++ Extensions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
